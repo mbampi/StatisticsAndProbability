6830971759ef7205a158833daee73a0c5c4d14d8
Matheus Dussin Bampi <matheusbampi@gmail.com>
2019-05-25 21:05:37 -0300

    included standardizing

--- Main.py
+++ Main.py
@@ -33,6 +33,10 @@ def hypergeometric_distribution(x: float, n: float, N: float, N1: float):
     return (combination(N1, x) * combination(N2, float(n - x))) / combination(N, n)
 
 
+def standardizing(z, mean, std):
+    return (z-mean)/std
+
+
 def mean_confidence_interval(sample_mean, sample_size, confidence_level, v=None, standard_deviation=None):
     if sample_size > 30 or standard_deviation is not None:
         z = normal_distribution_area(confidence_level/2)
@@ -74,7 +78,8 @@ while True:
           "5- Distribuicao Binomial \n "
           "6- Distribuicao Poisson \n "
           "7- Distribuicao Hipergeometrica \n"
-          "8- Normal Distribution Area \n"
+          "8- Padronizar (X -> Z) \n"
+          "9- Normal Distribution Area \n"
           "0- Sair")
     op = int(input("-> "))
 
@@ -168,10 +173,20 @@ while True:
         print(" = " + str(result))
 
     elif op == 8:
+        print("Padronizar (X->Z)")
+
+        x = float(input("x = "))
+        mean = float(input("media = "))
+        std = float(input("desvio padrao = "))
+
+        z = round(standardizing(x), 5)
+        print("z=" + str(z))
+
+    elif op == 9:
         print("Normal Distribution Area - Table")
 
-        x = float(input("z = "))
-        area = round(normal_distribution_area(x), 5)
+        z = float(input("z = "))
+        area = round(normal_distribution_area(z), 5)
         print("Area(0, z)=" + str(area))
 
     else:
